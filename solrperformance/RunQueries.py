70f9aedd54c602f7313b8d105a98634794c53a9c
Guenter Hipler <guenter.hipler@unibas.ch>
2017-12-08 17:48:31 +0100

    more details in text summary after performance run

--- solrperformance/RunQueries.py
+++ solrperformance/RunQueries.py
@@ -14,7 +14,7 @@ class RunQueries():
     def __init__(self,
                  mongoUser = None,
                  mongoPassword = None,
-                 time = None,
+                 minTime = None,
                  config = None,
                  addparams = "",
                  addnote = "",
@@ -35,22 +35,62 @@ class RunQueries():
             self.solrURL = self.config.getConfig()["SOLR"]["host"]
         else:
             self.solrURL = solrHost
-        self.minimumLiveTime = time
+
+        if minTime is None:
+            self.minimumLiveTime = 0
+        else:
+            self.minimumLiveTime = minTime
 
         self.timestampFile = open("solrperformance/timestamps.txt","a")
+        self.timestampMongo = "testtime_" + self.currentTime
 
-        self.timestampFile.write("testtime_" + self.currentTime + os.linesep)
+        self.timestampFile.write(self.timestampMongo + os.linesep)
         self.timestampFile.flush()
         self.timestampFile.close()
         self.additionalparams = addparams
 
 
+        self.startTime = time.strftime('%H:%M:%S')
+        self.totalQueryTime = 0
+        self.totalNumberRequests = 0
+        self.timeCollection = {}
+        self.initializeCollecCurrentQueryTime()
+
+
+
+    def getURL(self):
+        return self.solrURL
+
+    def getMongoTimeStamp(self):
+        return self.timestampMongo
+
+    def getAverTime(self):
+        return self.totalQueryTime / self.totalNumberRequests
+
+    def getMinTime(self):
+        return self.minimumLiveTime
+
+
+    def getStartTime(self):
+        return self.startTime
+
+    def getDistribution(self):
+        distribution = []
+        for k, v in self.timeCollection.items():
+            distribution.append(" (" + k + "=>" + str(v) + ") ")
+
+        return ''.join(distribution)
+
+
+
+
+
 
     def startRunning(self):
 
-        startTime = time.strftime('%H:%M:%S')
 
-        if self.minimumLiveTime is None:
+
+        if self.minimumLiveTime == 0:
             filter = {}
         else:
             filter = {"time": {"$gt": (int)(self.minimumLiveTime)}}
@@ -65,6 +105,11 @@ class RunQueries():
                 io = StringIO(result.text)
                 myJson = json.load(io)
                 queryTime = myJson["responseHeader"]["QTime"]
+
+                self.totalQueryTime += queryTime
+                self.totalNumberRequests += 1
+                self.collecCurrentQueryTime(queryTime)
+
                 numberHits = myJson["response"]["numFound"]
 
                 responseObject = self.mongoWrapper.getResponseObject(docId)
@@ -101,12 +146,100 @@ class RunQueries():
             except Exception as ex:
                 print (ex)
 
-        endtime = time.strftime('%H:%M:%S')
-        durationFile = open("solrperformance/duration.txt","a")
 
-        durationFile.write("duration of test: " + startTime + " / " + endtime + os.linesep)
-        durationFile.flush()
-        durationFile.close()
+    def collecCurrentQueryTime(self, queryTime):
+
+        if queryTime >= 0 and queryTime <= 100:
+            self.timeCollection['0_100'] += 1
+        elif queryTime >= 101 and queryTime <= 200:
+            self.timeCollection['100_200'] += 1
+        elif queryTime >= 201 and queryTime <= 300:
+            self.timeCollection['200_300'] += 1
+        elif queryTime >= 301 and queryTime <= 400:
+            self.timeCollection['300_400'] += 1
+        elif queryTime >= 401 and queryTime <= 500:
+            self.timeCollection['400_500'] += 1
+        elif queryTime >= 501 and queryTime <= 600:
+            self.timeCollection['500_600'] += 1
+        elif queryTime >= 601 and queryTime <= 700:
+            self.timeCollection['600_700'] += 1
+        elif queryTime >= 701 and queryTime <= 800:
+            self.timeCollection['700_800'] += 1
+        elif queryTime >= 801 and queryTime <= 900:
+            self.timeCollection['800_900'] += 1
+        elif queryTime >= 901 and queryTime <= 1000:
+            self.timeCollection['900_1000'] += 1
+        elif queryTime >= 1001 and queryTime <= 1200:
+            self.timeCollection['1000_1200'] += 1
+        elif queryTime >= 1201 and queryTime <= 1400:
+            self.timeCollection['1200_1400'] += 1
+        elif queryTime >= 1401 and queryTime <= 1600:
+            self.timeCollection['1400_1600'] += 1
+        elif queryTime >= 1601 and queryTime <= 1800:
+            self.timeCollection['1600_1800'] += 1
+        elif queryTime >= 1801 and queryTime <= 2000:
+            self.timeCollection['1800_2000'] += 1
+        elif queryTime >= 2001 and queryTime <= 2400:
+            self.timeCollection['2000_2400'] += 1
+        elif queryTime >= 2401 and queryTime <= 2800:
+            self.timeCollection['2400_2800'] += 1
+        elif queryTime >= 2801 and queryTime <= 3200:
+            self.timeCollection['2800_3200'] += 1
+        elif queryTime >= 3201 and queryTime <= 3800:
+            self.timeCollection['3200_3800'] += 1
+        elif queryTime >= 3801 and queryTime <= 4400:
+            self.timeCollection['3800_4400'] += 1
+        elif queryTime >= 4401 and queryTime <= 5000:
+            self.timeCollection['4400_5000'] += 1
+        elif queryTime >= 5001 and queryTime <= 6000:
+            self.timeCollection['5000_6000'] += 1
+        elif queryTime >= 6001 and queryTime <= 7000:
+            self.timeCollection['6000_7000'] += 1
+        elif queryTime >= 7001 and queryTime <= 8000:
+            self.timeCollection['7000_8000'] += 1
+        elif queryTime >= 8001 and queryTime <= 9000:
+            self.timeCollection['8000_9000'] += 1
+        elif queryTime >= 9001 and queryTime <= 10000:
+            self.timeCollection['9000_10000'] += 1
+        elif queryTime >= 10001 and queryTime <= 15000:
+            self.timeCollection['10000_15000'] += 1
+        elif queryTime >= 15001 and queryTime <= 20000:
+            self.timeCollection['15000_20000'] += 1
+        elif queryTime >= 20001:
+            self.timeCollection['gt20000'] += 1
+
+
+    def initializeCollecCurrentQueryTime(self):
+
+        self.timeCollection['0_100'] = 0
+        self.timeCollection['100_200'] = 0
+        self.timeCollection['200_300'] = 0
+        self.timeCollection['300_400'] = 0
+        self.timeCollection['400_500'] = 0
+        self.timeCollection['500_600'] = 0
+        self.timeCollection['600_700'] = 0
+        self.timeCollection['700_800'] = 0
+        self.timeCollection['800_900'] = 0
+        self.timeCollection['900_1000'] = 0
+        self.timeCollection['1000_1200'] = 0
+        self.timeCollection['1200_1400'] = 0
+        self.timeCollection['1400_1600'] = 0
+        self.timeCollection['1600_1800'] = 0
+        self.timeCollection['1800_2000'] = 0
+        self.timeCollection['2000_2400'] = 0
+        self.timeCollection['2400_2800'] = 0
+        self.timeCollection['2800_3200'] = 0
+        self.timeCollection['3200_3800'] = 0
+        self.timeCollection['3800_4400'] = 0
+        self.timeCollection['4400_5000'] = 0
+        self.timeCollection['5000_6000'] = 0
+        self.timeCollection['6000_7000'] = 0
+        self.timeCollection['7000_8000'] = 0
+        self.timeCollection['8000_9000'] = 0
+        self.timeCollection['9000_10000'] = 0
+        self.timeCollection['10000_15000'] = 0
+        self.timeCollection['15000_20000'] = 0
+        self.timeCollection['gt20000'] = 0
 
 
 
@@ -133,7 +266,32 @@ if __name__ == '__main__':
                         args.note,
                         args.solrhost)
 
-    runner.startRunning()
+    try:
+        runner.startRunning()
+    except Exception as ex:
+        print (ex)
+
+    endtime = time.strftime('%H:%M:%S')
+    summaryFile = open("solrperformance/summary.txt","a")
+
+    stopTime = time.strftime('%H:%M:%S')
+
+    summary = 'summary: startTime:{STARTTIME}   stopTime:{STOPTIME} solrurl:{URL}   timestamp:{TIMESTAMP}   averageTime:{AVERTIME}  mintime:{MINTIME}  distribution:{DISTRIBUTION}'.format(
+        STARTTIME=runner.getStartTime(),
+        STOPTIME=stopTime,
+        URL=runner.getURL(),
+        TIMESTAMP=runner.getMongoTimeStamp(),
+        AVERTIME=runner.getAverTime(),
+        MINTIME=runner.getMinTime(),
+        DISTRIBUTION=runner.getDistribution()
+
+
+    )
+
+    summaryFile.write(summary + os.linesep + os.linesep)
+    summaryFile.flush()
+    summaryFile.close()
+
 
 
 
